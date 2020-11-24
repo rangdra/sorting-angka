740fc1d448a486f3cc4fada2409fb652ff409cea
Rangdra22 <63014773+Rangdra22@users.noreply.github.com>
2020-11-24 12:47:30 +0700

    fg

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const search = document.querySelector(".search-button");
 let angka = [];
 search.addEventListener("click", function () {
   const inputKeyword = document.querySelector(".input-keyword");
+  const angka = inputKeyword.value;
+  const arrAngka = angka.split(" ");
   const sort = document.querySelector(".sort");
-  const angkaFix = inputKeyword.value;
-  angka.push(angkaFix);
 
-  angka.sort((a, b) => a - b);
-  sort.innerHTML = `<ul>
-        <li>${angka.join(" ")}</li>
-  </ul>`;
+  const sorting = arrAngka.sort((a, b) => a - b);
+  console.log(sorting);
+  sort.innerHTML = `<li>${sorting.join(",")}</li>`;
+  // const angkaFix = inputKeyword.value;
+  // angka.push(angkaFix);
+
+  // angka.sort((a, b) => a - b);
+  // sort.innerHTML = `<ul>
+  //       <li>${angka.join(" ")}</li>
+  // </ul>`;
 });
